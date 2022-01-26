97666a7e3f523c17b598b07b59e05a22f4ccae3d
Kartik Singh <kssingh@hrl.com>
2022-01-26 09:23:59 -0800

    Add a function for removing zeros
    Also modify the example programs to take input in a loop instead of quitting after the first input.

--- example/example.c
+++ example/example.c
@@ -1,6 +1,7 @@
 #include "libcalc.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 void die(char *msg) {
   printf("%s\n", msg);
@@ -12,23 +13,29 @@ int main(int argc, char **argv) {
 
   char source[256];
   printf("> ");
-  fgets(source, sizeof(source), stdin);
 
-  expr_type expr;
-  if (calc_parse(source, &expr) != ERR_SUCCESS)
-    die("unable to parse expression");
+  while (fgets(source, sizeof(source), stdin) != NULL) {
+    expr_type expr;
+    if (calc_parse(source, &expr) != ERR_SUCCESS)
+      die("unable to parse expression");
 
-  char *result;
+    char *result;
 
-  expr_type simplified_expr;
-  calc_simplify(expr, &simplified_expr);
-  
-  if (calc_expression_to_string(simplified_expr, &result) != ERR_SUCCESS)
-    die("unable to print expression to string");
+    expr_type simplified_expr;
 
-  printf("\n%s\n", result);
+    if (argc == 2 && !strcmp(argv[1], "remove-zeros"))
+      calc_remove_zeros(expr, &simplified_expr);
+    else
+      calc_simplify(expr, &simplified_expr);
 
-  calc_release_handle(expr);
-  calc_release_handle(simplified_expr);
+    if (calc_expression_to_string(simplified_expr, &result) != ERR_SUCCESS)
+      die("unable to print expression to string");
+
+    printf("\n%s\n> ", result);
+
+    calc_release_handle(expr);
+    calc_release_handle(simplified_expr);
+  }
+
   return 0;
 }

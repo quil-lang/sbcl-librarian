fb8bf76753dbde68d0056999ee6da37c43907e33
Charles Zhang <czhang@hrl.com>
2022-02-03 15:04:02 -0800

    Fix again.

--- example/script.lisp
+++ example/script.lisp
@@ -2,6 +2,9 @@
 
 (asdf:load-system '#:libcalc)
 
+
+(in-package #:sbcl-librarian/example/libcalc)
+
 (build-bindings libcalc ".")
 (build-python-bindings libcalc ".")
 (build-core-and-die libcalc ".")

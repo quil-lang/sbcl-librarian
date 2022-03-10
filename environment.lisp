a3e334232e5bdc66cecd5726b3b9fc5845121d94
Charles Zhang <czhang@hrl.com>
2022-03-09 16:13:42 -0800

    Provide a wayto disable the debugger as well.
    As well as fixing up some of the ENABLE-DEBUGGER code.

--- environment.lisp
+++ environment.lisp
@@ -8,23 +8,23 @@
 (defun enable-debugger ()
   (setq sb-ext:*invoke-debugger-hook*
         (lambda (condition old-hook)
-          (let ((sb-ext:*invoke-debugger-hook* nil)
-                (session sb-thread::*session*)
-                (current-thread sb-thread::*current-thread*))
+          (let ((sb-ext:*invoke-debugger-hook* nil))
             ;; This is a huge hack messing with the internals of the
             ;; session thread state in SBCL. SBCL doesn't know that
             ;; foreign callback threads could ever want to hijack the
             ;; main thread and become interactive themselves for
-            ;; debugging.
-            (symbol-macrolet ((interactive-threads
-                                (sb-thread::session-interactive-threads session)))
-              (let ((this-thread current-thread))
-                (unless (member current-thread interactive-threads)
-                  (setf interactive-threads
-                        (list current-thread)))))
+            ;; debugging. The session data-structure is also usually
+            ;; protected by a lock, which we're not doing here because
+            ;; this is a hack.
+            (push sb-thread::*current-thread*
+                  (sb-thread::session-interactive-threads sb-thread::*session*))
             (invoke-debugger condition))))
   (sb-ext:enable-debugger))
 
+(defun disable-debugger ()
+  (sb-ext:disable-debugger))
+
 (define-api environment (:function-prefix "")
   (:function
-   (("lisp_enable_debugger" enable-debugger) :void ())))
+   (("lisp_enable_debugger" enable-debugger) :void ())
+   (("lisp_disable_debugger" disable-debugger) :void ())))

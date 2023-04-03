8100eaecb32cd438c19886c9026088f39cdfa81b
Colin O'Keefe <colin@cicadas.surf>
2023-04-03 11:24:28 -0700

    Fix memory leak in callback eg; augment callback eg with arguments

--- examples/callback/example.py
+++ examples/callback/example.py
@@ -3,12 +3,13 @@ import libcallback
 import sys
 
 
-@CFUNCTYPE(c_char_p)
-def foo():
-    return "it works".encode("utf-8")
+@CFUNCTYPE(None, c_char_p, POINTER(c_char))
+def foo(s, outbuffer):
+    writable = cast(outbuffer, POINTER(c_char * 100))
+    writable.contents.value = (s.decode("utf-8") + " it works!").encode("utf-8")
 
 def moo():
-    raw_str = c_char_p()
+    raw_str = create_string_buffer(100)
     libcallback.callback_call_callback(foo, raw_str)
     return raw_str.value.decode("utf-8")
 

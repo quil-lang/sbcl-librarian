3c0c56174fee18b4b96cc3415adc278aa567bf67
Kartik Singh <kssingh@hrl.com>
2024-01-11 09:01:23 -0800

    Export new symbols

--- libsbcl/embed-core.lisp
+++ libsbcl/embed-core.lisp
@@ -18,4 +18,4 @@
       (shared-lib-suffix (second (uiop:command-line-arguments))))
   (setf (extern-alien "sbcl_runtime" (* t)) (make-alien-string runtime-path))
   (trace sb-alien::initialize-alien-callable-symbol)
-  (save-lisp-and-die (concatenate 'string "libsbcl" shared-lib-suffix) :executable t :callable-exports '(square)))
+  (save-lisp-and-die (concatenate 'string "libsbcl" shared-lib-suffix) :executable t :callable-exports '(funcall0-by-name set-argv load-array)))

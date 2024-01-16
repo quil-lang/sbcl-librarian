20b556d5b3d02df91bf0997572ff903f6890774a
Kartik Singh <kssingh@hrl.com>
2024-01-16 09:41:30 -0800

    Try using GetModuleFileNameA

--- libsbcl/libsbcl.c
+++ libsbcl/libsbcl.c
@@ -14,66 +14,24 @@ void (*load_array)(char *data, int size);
 
 #define BUF_SIZE 1024
 
-#ifdef _WIN32
-void NtPathToDosPath(char *pszFilename) {
-    // Translate path with device name to drive letters.
-    TCHAR szTemp[BUFSIZE];
-    szTemp[0] = '\0';
-
-    if (GetLogicalDriveStrings(BUFSIZE-1, szTemp))
-    {
-        TCHAR szName[MAX_PATH];
-        TCHAR szDrive[3] = TEXT(" :");
-        BOOL bFound = FALSE;
-        TCHAR* p = szTemp;
-
-        do
-        {
-            // Copy the drive letter to the template string
-            *szDrive = *p;
-
-            // Look up each device name
-            if (QueryDosDevice(szDrive, szName, MAX_PATH))
-            {
-                size_t uNameLen = _tcslen(szName);
-
-                if (uNameLen < MAX_PATH)
-                {
-                    bFound = _tcsnicmp(pszFilename, szName, uNameLen) == 0
-                        && *(pszFilename + uNameLen) == _T('\\');
-
-                    if (bFound)
-                    {
-                        // Reconstruct pszFilename using szTempFile
-                        // Replace device path with DOS path
-                        TCHAR szTempFile[MAX_PATH];
-                        StringCchPrintf(szTempFile,
-                                MAX_PATH,
-                                TEXT("%s%s"),
-                                szDrive,
-                                pszFilename+uNameLen);
-                        StringCchCopyN(pszFilename, MAX_PATH+1, szTempFile, _tcslen(szTempFile));
-                    }
-                }
-            }
-
-            // Go to the next NULL character.
-            while (*p++);
-        } while (!bFound && *p); // end of string
-    }
-}
-#endif
-
 __attribute__((constructor))
 void init(void)
 {
-   char *lib_path;
+    char *lib_path;
 #ifdef _WIN32
-   char buf[LIB_PATH_BUF_SIZE];
+    char buf[BUF_SIZE];
+    HMODULE mod;
 
-   GetMappedFileNameA(GetCurrentProcess(), init, buf, BUF_SIZE);
-   NtPathToDosPath(buf);
-   lib_path = buf;
+    GetModuleHandleEx(
+        GET_MODULE_HANDLE_EX_FLAG_FROM_ADDRESS | GET_MODULE_HANDLE_EX_FLAG_UNCHANGED_REFCOUNT,
+        (LPCTSTR) init,
+        &mod
+    );
+    GetModuleFileNameA(mod, buf, BUF_SIZE);
+#if 0
+    GetMappedFileNameA(GetCurrentProcess(), init, buf, BUF_SIZE);
+#endif
+    lib_path = buf;
 #else
     Dl_info info;
 

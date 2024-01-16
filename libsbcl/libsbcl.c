77228cd6a726016d52345446d5b98f908a00f82a
Kartik Singh <kssingh@hrl.com>
2024-01-16 08:26:25 -0800

    Make libsbcl build on Windows

--- libsbcl/libsbcl.c
+++ libsbcl/libsbcl.c
@@ -1,4 +1,9 @@
+#ifdef _WIN32
+#include <Windows.h>
+#include <psapi.h>
+#else
 #include <dlfcn.h>
+#endif
 #include <stdio.h>
 
 extern int initialize_lisp(int argc, char *argv[], char *envp[]);
@@ -7,14 +12,25 @@ int (*funcall0_by_name)(const char *name);
 void (*set_argv)(int argc, char **argv);
 void (*load_array)(char *data, int size);
 
+#define LIB_PATH_BUF_SIZE 1024
+
 __attribute__((constructor))
 void init(void)
 {
+   char *lib_path;
+#ifdef _WIN32
+   char buf[LIB_PATH_BUF_SIZE];
+
+   GetMappedFileNameA(GetCurrentProcess(), init, buf, LIB_PATH_BUF_SIZE);
+   lib_path = buf;
+#else
     Dl_info info;
 
     dladdr(init, &info);
-    printf("%s\n", info.dli_fname);
-    const char *init_args[] = {"", "--core", info.dli_fname, "--no-userinit", "--noinform"};
+    lib_path = info.dli_fname;
+#endif
+    printf("%s\n", lib_path);
+    char *init_args[] = {"", "--core", lib_path, "--no-userinit", "--noinform"};
     int res = initialize_lisp(5, init_args, NULL);
     printf("hello from library load time, initialize_lisp returned %d\n", res);
 }

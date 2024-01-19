850d644f4b77b3c2ce1fb4d3bdf5d3c921cb407b
Kartik Singh <kssingh@hrl.com>
2024-01-19 11:04:03 -0800

    Untrace initialize-alien-callable-symbol

--- libsbcl/embed-core.lisp
+++ libsbcl/embed-core.lisp
@@ -7,7 +7,6 @@
 (let ((runtime-path (first (uiop:command-line-arguments)))
       (lib-filename (second (uiop:command-line-arguments))))
   (setf (sb-alien:extern-alien "sbcl_runtime" (* t)) (sb-alien:make-alien-string runtime-path))
-  (trace sb-alien::initialize-alien-callable-symbol)
   (sbcl-librarian:build-core-and-die sbcl-librarian::libsbcl "."
 				     :core-name lib-filename
 				     :executable t))

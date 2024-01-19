147ca76ea2a04d0578b730515cd259add3b75ca3
Kartik Singh <kssingh@hrl.com>
2024-01-19 10:50:32 -0800

    Build core exporting libsbcl functions

--- libsbcl/embed-core.lisp
+++ libsbcl/embed-core.lisp
@@ -6,8 +6,8 @@
 
 (let ((runtime-path (first (uiop:command-line-arguments)))
       (lib-filename (second (uiop:command-line-arguments))))
-  (setf (extern-alien "sbcl_runtime" (* t)) (make-alien-string runtime-path))
+  (setf (sb-alien:extern-alien "sbcl_runtime" (* t)) (sb-alien:make-alien-string runtime-path))
   (trace sb-alien::initialize-alien-callable-symbol)
-  (save-lisp-and-die lib-filename
-                     :executable t
-                     :callable-exports '(funcall0-by-name set-argv load-array-as-system load-shared-object)))
+  (sbcl-librarian:build-core-and-die sbcl-librarian::libsbcl "."
+				     :core-name lib-filename
+				     :executable t))

8edf6783f9579f8dbab05edf80b03da27b3a57da
Kartik Singh <kssingh@hrl.com>
2024-01-22 09:03:39 -0800

    Copy lib path to heap in entry point

--- libsbcl/entry_point.c
+++ libsbcl/entry_point.c
@@ -5,6 +5,8 @@
 #else
 #include <dlfcn.h>
 #endif
+#include <stdlib.h>
+#include <string.h>
 
 #define BUF_SIZE 1024
 
@@ -22,7 +24,10 @@ BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved)
         char *init_args[] = {"", "--core", lib_path, "--no-userinit", "--noinform"};
 
         initialize_lisp(sizeof(init_args) / sizeof(init_args[0]), init_args, 0);
-        sbcl_runtime = lib_path;
+
+        int n = strlen(lib_path) * sizeof(char);
+        sbcl_runtime = malloc(n);
+        memcpy(sbcl_runtime, lib_path, n);
         sbcl_runtime_home = dir_name(sbcl_runtime);
     }
 
@@ -37,7 +42,10 @@ void init(void)
     char *init_args[] = {"", "--core", info.dli_fname, "--no-userinit", "--noinform"};
 
     initialize_lisp(sizeof(init_args) / sizeof(init_args[0]), init_args, 0);
-    sbcl_runtime = info.dli_fname;
+
+    int n = strlen(info.dli_fname) * sizeof(char);
+    sbcl_runtime = malloc(n);
+    memcpy(sbcl_runtime, info.dli_fname, n);
     sbcl_runtime_home = dir_name(sbcl_runtime);
 }
 #endif

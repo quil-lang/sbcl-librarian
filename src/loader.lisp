45fa3c1a984e0bcdb2776dae0ea5ed1dddf88306
Kartik Singh <kssingh@hrl.com>
2024-01-23 09:12:27 -0800

    Add LOAD-ARRAY-AS-SYSTEM and REQUIRE to loader API

--- src/loader.lisp
+++ src/loader.lisp
@@ -7,6 +7,27 @@
   (let ((*initialize-callables-p* t))
     (load pathname)))
 
+(defun load-array-as-system  (data size system-name)
+  "Assuming DATA is a pointer to an array of SIZE bytes constituting a
+FASL or Common Lisp source code for the entire ASDF system named
+SYSTEM-NAME, dump the array to a temporary file and then load it if
+SYSTEM-NAME has not already been loaded into the current
+image. Additionally, initialize any alien callable symbols while
+loading."
+  (unless (asdf:component-loaded-p system-name)
+    (uiop:with-temporary-file (:stream stream :pathname filename :direction :io :element-type 'unsigned-byte)
+      (loop :for i :from 0 :below size
+            :do (write-byte (sb-alien:deref (sb-alien:cast data (* (sb-alien:unsigned 8)))
+					    i)
+			    stream))
+      (finish-output stream)
+      (let ((sbcl-librarian::*initialize-callables-p* t))
+        (load filename)))
+    (asdf:register-immutable-system system-name)
+    (values)))
+
 (define-api loader (:function-prefix "")
   (:function
-   (("lisp_load" lisp-load) :void ((pathname :string)))))
+   (("lisp_load" lisp-load) :void ((pathname :string)))
+   (("lisp_load_array_as_system" load-array-as-system) :void ((data :pointer) (size :int) (system-name :string)))
+   (("lisp_require" require) :void ((module-name :string)))))

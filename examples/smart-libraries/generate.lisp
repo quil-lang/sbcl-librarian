4f42378c9cd6121289ce75f5675f577111dd6a04
Kartik Singh <kssingh@hrl.com>
2024-01-25 09:14:42 -0800

    Fixup generate scripts

--- examples/smart-libraries/generate.lisp
+++ examples/smart-libraries/generate.lisp
@@ -1,8 +1,6 @@
-(require "asdf")
-
 (asdf:load-system :sbcl-librarian)
 (asdf:load-system :libminiquilc)
 (asdf:load-system :libminiqvm)
 
-(sbcl-librarian:create-fasl-library-cmake-project "libminiquilc" libminiquilc:libminiquilc "build/libquilc")
-(sbcl-librarian:create-fasl-library-cmake-project "libminiqvm" libminiqvm:libminiqvm "build/libqvm")
+(sbcl-librarian:create-fasl-library-cmake-project "libminiquilc" libminiquilc:libminiquilc "build/libminiquilc" :base-library-name "libbase")
+(sbcl-librarian:create-fasl-library-cmake-project "libminiqvm" libminiqvm:libminiqvm "build/libminiqvm" :base-library-name "libbase")
--- examples/smart-libraries/libbase/generate.lisp
+++ examples/smart-libraries/libbase/generate.lisp
@@ -1,6 +1,4 @@
-(require "asdf")
-
 (asdf:load-system :libbase)
 
-(sbcl-librarian:build-bindings libbase:libbase ".")
-(sbcl-librarian:build-core-and-die libbase:libbase ".")
+(sbcl-librarian:build-bindings libbase:libbase "build/libbase/")
+(sbcl-librarian:build-core-and-die libbase:libbase "build/libbase/")

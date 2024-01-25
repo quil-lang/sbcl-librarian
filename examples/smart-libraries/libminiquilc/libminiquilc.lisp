e568fa7b67d00d50fac5054484e7e3358faa18d3
Kartik Singh <kssingh@hrl.com>
2024-01-25 09:14:04 -0800

    Add error maps to bindings etc.

--- examples/smart-libraries/libminiquilc/libminiquilc.lisp
+++ examples/smart-libraries/libminiquilc/libminiquilc.lisp
@@ -4,8 +4,17 @@
 
 (sbcl-librarian:define-handle-type quil-program-type "quil_program")
 
-(sbcl-librarian:define-library libminiquilc (:function-linkage "MINIQUILC_API"
+(define-enum-type error-type "miniquilc_err_t"
+  ("ERR_SUCCESS" 0)
+  ("ERR_FAIL" 1))
+(define-error-map error-map error-type 0
+  ((t (lambda (condition)
+        (declare (ignore condition))
+        (return-from error-map 1)))))
+
+(sbcl-librarian:define-library libminiquilc (:error-map error-map
+                                             :function-linkage "MINIQUILC_API"
                                              :function-prefix "miniquilc_")
-  (:type quil-program-type)
+  (:type quil-program-type error-type)
   (:function
    (("parse_quil" cl-quil:safely-parse-quil) quil-program-type ((source :string)))))
--- examples/smart-libraries/libminiqvm/libminiqvm.lisp
+++ examples/smart-libraries/libminiqvm/libminiqvm.lisp
@@ -3,11 +3,20 @@
 (in-package #:libminiqvm)
 
 (sbcl-librarian:define-handle-type qvm-type "qvm")
-(sbcl-librarian:define-handle-type quil-program-type "quil_program")
+(sbcl-librarian:define-handle-type quil-program-type "qvm_quil_program")
 
-(sbcl-librarian:define-library libminiqvm (:function-linkage "MINIQVM_"
+(define-enum-type error-type "miniqvm_err_t"
+  ("ERR_SUCCESS" 0)
+  ("ERR_FAIL" 1))
+(define-error-map error-map error-type 0
+  ((t (lambda (condition)
+        (declare (ignore condition))
+        (return-from error-map 1)))))
+
+(sbcl-librarian:define-library libminiqvm (:error-map error-map
+                                           :function-linkage "MINIQVM_"
                                            :function-prefix "miniqvm_")
-  (:type qvm-type quil-program-type)
+  (:type qvm-type quil-program-type error-type)
   (:function
    (("run_program" qvm:run-program) qvm-type ((num-qubits :int) (program quil-program-type)))
    (("amplitudes" qvm::amplitudes) :string ((qvm qvm-type)))))

b956fc0a9582c52a1442f4f9e9e6fcb9a800e48b
Kartik Singh <kssingh@hrl.com>
2024-04-04 03:11:59 -0700

    cleanup

--- src/api.lisp
+++ src/api.lisp
@@ -38,9 +38,7 @@ All Lisp calls will get wrapped in a block named NAME, within which a HANDLER-BI
         (list (values (first callable-name) (second callable-name)))
         (symbol (values (lisp-to-c-name callable-name) callable-name))
         (string (values callable-name (c-to-lisp-name callable-name))))
-    (let ((thing (list (concatenate 'string prefix c-name) lisp-name)))
-      (format t "~A~%" thing)
-      thing)))
+    (list (concatenate 'string prefix c-name) lisp-name)))
 
 (defun coerce-to-c-name (name)
   (typecase name

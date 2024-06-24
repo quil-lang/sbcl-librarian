ced9bdb18f33892b9be6e3858d04d67a0b500dc1
Kartik Singh <kssingh@hrl.com>
2024-06-24 13:47:08 -0700

    Run Python example in CI

--- examples/libcalc/script.lisp
+++ examples/libcalc/script.lisp
@@ -2,9 +2,15 @@
 
 (asdf:load-system '#:libcalc)
 
+(when (uiop:getenv "CI")
+  (push :github-ci *features*))
 
 (in-package #:sbcl-librarian/example/libcalc)
 
-(build-bindings libcalc ".")
-(build-python-bindings libcalc ".")
-(build-core-and-die libcalc "." :compression nil)
+(let ((sbcl-librarian::*non-static-lossage-handler* t))
+  (build-bindings libcalc ".")
+  (build-python-bindings libcalc "." #+github-ci :library-path
+                                     #+(and github-ci win32) (concatenate 'string (uiop:getenv "MSYSTEM_PREFIX") "/bin/libcalc.dll")
+                                     #+(and github-ci linux) "/usr/local/lib/libcalc.so"
+                                     #+(and github-ci darwin) nil)
+  (build-core-and-die libcalc "." :compression nil))

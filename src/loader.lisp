b6a6dfa4953fda8288571f34a147c77e38ae32cb
Kartik Singh <kssingh@hrl.com>
2024-06-28 15:01:31 -0700

    Add more functions to loader

--- src/loader.lisp
+++ src/loader.lisp
@@ -4,9 +4,36 @@
 (in-package #:sbcl-librarian)
 
 (defun lisp-load (pathname)
-  (let ((*initialize-callables-p* t))
-    (load pathname)))
+  (let ((*initialize-callables-p* t)
+        (initial-thread sb-thread::*initial-thread*))
+    (setf sb-thread::*initial-thread* sb-thread:*current-thread*)
+    (unwind-protect
+         (locally
+             (declare (sb-ext:muffle-conditions sb-kernel:redefinition-warning))
+           (load pathname))
+      (setf sb-thread::*initial-thread* initial-thread))))
+
+(defun load-array-as-system  (data size system-name)
+  "Assuming DATA is a pointer to an array of SIZE bytes constituting a
+FASL or Common Lisp source code for the entire ASDF system named
+SYSTEM-NAME, dump the array to a temporary file and then load it if
+SYSTEM-NAME has not already been loaded into the current
+image. Additionally, initialize any alien callable symbols while
+loading."
+  (unless (asdf:component-loaded-p system-name)
+    (uiop:with-temporary-file (:stream stream :pathname filename :direction :io :element-type 'unsigned-byte)
+      (loop :for i :from 0 :below size
+            :do (write-byte (sb-alien:deref (sb-alien:cast data (* (sb-alien:unsigned 8)))
+					    i)
+			    stream))
+      (finish-output stream)
+      (lisp-load filename))
+    (asdf:register-immutable-system system-name)
+    (values)))
 
 (define-api loader (:function-prefix "")
   (:function
-   (("lisp_load" lisp-load) :void ((pathname :string)))))
+   (("lisp_load" lisp-load) :void ((pathname :string)))
+   (("lisp_load_array_as_system" load-array-as-system) :void ((data :pointer) (size :int) (system-name :string)))
+   (("lisp_require" require) :void ((module-name :string)))
+   (("lisp_load_shared_object" load-shared-object) :void ((pathname :string)))))
